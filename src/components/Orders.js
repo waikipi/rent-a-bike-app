9c9ed6b6d27dd48f9ef588426ed343824a69738d
waikipi <galianez@gmail.com>
2023-03-07 19:02:06 +0100

    h

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -11,7 +11,7 @@ export default function Orders() {
   useEffect(() => {
     //fetch(`http://localhost:3001/orders/${biker}`)
 	//fetch(`${MY_DOMAIN}/${biker}`)
-	fetch(`orders/${biker}`)
+	fetch(`/orders/${biker}`)
       .then((res) => res.json())
       .then((data) => {
 		setOrderData(data)
