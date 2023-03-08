76fbe978570e4779aaeb18dc30838086e48430fc
waikipi <galianez@gmail.com>
2023-03-08 11:21:43 +0100

    d

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const publicPath = join(__dirname, '../build');
 app.use(express.static(publicPath));
-app.get('*', (req, res) => {
+app.get('https://rent-a-bike-app.herokuapp.com', (req, res) => {
    res.sendFile(join(publicPath, '../build/index.html'));
 });
 
